187d0b438665f09d835128cacbef86db5f4905e2
ShiversVert <38329790+ShiversVert@users.noreply.github.com>
2018-05-02 17:50:09 +0200

    fonction appartient

--- fonctions_prog.h
+++ fonctions_prog.h
@@ -4,6 +4,6 @@
 Liste ajout_pcc_triee(long i, Liste pcc_triee, SOMMET * l_sommet);
 ARC creer_arc(long arrivee,	double cout);
 void ajouter_arc(ARC arc_init, ARC arc_a_ajouter);
-SOMMET creer_sommet(char* nom, double x, double y ,ARC voisins, double pcc,	long pere);
-
+SOMMET creer_sommet(char* nom, double x, double y ,ARC voisins);
+int appartient(Liste sommet_visites, long arrivee);
 
