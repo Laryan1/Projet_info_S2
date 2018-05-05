d061764b6f8a373e27efd6a9793e4e682e939f8c
Far4way <florianlaffont09300@gmail.com>
2018-05-05 12:58:18 +0200

    Delete fonctions_prog.h

--- fonctions_prog.h
+++
@@ -1,9 +0,0 @@
-#include "types.h"
-#include "liste.h"
-
-Liste ajout_pcc_triee(long i, Liste pcc_triee, SOMMET * l_sommet);
-ARC creer_arc(long arrivee,	double cout);
-void ajouter_arc(ARC arc_init, ARC arc_a_ajouter);
-SOMMET creer_sommet(char* nom, double x, double y ,ARC voisins);
-int appartient(Liste sommet_visites, long arrivee);
-
